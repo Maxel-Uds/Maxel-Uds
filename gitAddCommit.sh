fe01d7769ebb2169180e767bc04e5245cded2f64
Maxel-Uds <maxellopes32@gmail.com>
2021-11-01 10:30:26 -0400

    Adicionando o comando push ao arquivo

--- gitAddCommit.sh
+++ gitAddCommit.sh
@@ -1,4 +1,10 @@
-echo "Digite a mensagem de commit:"
-read MENSAGEM
-git add .
-git commit -m "$MENSAGEM"
+read -p "Digite a mensagem de commit: " MENSAGEM
+
+if [ -n "$MENSAGEM" ];
+then
+	git add .
+	git commit -m "$MENSAGEM"
+	git push
+else
+	echo "ERRO: mensagem de commit vazia"
+fi
